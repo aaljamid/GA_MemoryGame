6ef1bb214fcf05bc4b0f0cf6a8817be4aee25800
aLi <aaljamid@emich.edu>
2020-01-15 01:51:16 -0500

    adding an array and js variables

--- memory_game/js/Main.js
+++ memory_game/js/Main.js
@@ -1,13 +1,38 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
+// let cardOne = "queen";
 
-let cardTwo = "king";
+// let cardTwo = "king";
 
-let cardThree = "queen";
+// let cardThree = "queen";
 
-let cardFour = "king";
+// let cardFour = "king";
 
-console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardThree);
+
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+let cardTwo = cards[2];
+
+cardsInPlay.push("cardOne");
+
+cardsInPlay.push("cardTwo");
+
+// alert("Hello, friends.");
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+  console.log("You Have Picked " + cardsInPlay);
+} else {
+  console.log("Try Again ");
+}
